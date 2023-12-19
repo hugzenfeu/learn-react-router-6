c9d88d76477bc680d0269c69d6705cc4b80b8fd4
Hugo Casanave <casanave.hugo@gmail.com>
2023-12-19 22:46:42 +0100

    step 01/09

--- 01 - Introduction to React Router/06 - Route, path, & element/index.jsx
+++ 01 - Introduction to React Router/06 - Route, path, & element/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
@@ -8,10 +8,17 @@ function App() {
   )
 }
 
+function Boats(){
+  return (
+    <h1>test</h1>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      
+      <Route path='/' element={<App/>}/>
+      <Route path='/boats' element={<Boats/>} />
     </Routes>
   </BrowserRouter>
 );
--- 01 - Introduction to React Router/09 - VanLife project bootstrapping/index.jsx
+++ 01 - Introduction to React Router/09 - VanLife project bootstrapping/index.jsx
@@ -1,28 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 /**
  * Challenge:
  * Bootstrap the VanLife project by creating the first 2 routes:
  * Home and About.
- * 
+ *
  * Also include the navbar that can link between the two routes.
  * For now, you'll either need to copy/paste the navbar code
  * to both Home and About pages, or you'll need to find a place
  * to put it where it can be shared between the two pages.
  * (Don't overthink this part - just do whatever is easiest for
  * you because we'll learn a better approach very soon)
- * 
+ *
  * Review challenge: do all the CSS yourself based on the design
  * linked in the slides.
  */
+function nav() {
+  return (
+    <div>
+      <h1>
+        <Link to="/Home">Home</Link>
+      </h1>
+      <h1>
+        <Link to="/About">About</Link>
+      </h1>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      {nav()}
+      <h1>Home !!</h1>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      {nav()}
+      <h1>About !!</h1>
+    </>
+  );
+}
 
 function App() {
   return (
-    <h1>Start here</h1>
-  )
+    <>
+      <h1>Start here</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
